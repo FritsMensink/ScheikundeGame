19986a01e6688dbf7c4e53a7f7cefa44fb74c663
Jonathan Karssen <jonathankarssen@gmail.com>
2015-12-10 23:05:12 +0100

    begin van periodiek systeem domein

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Element {
+    public int AtomischNummer;
+    public string Afkorting;
+    public string Naam;
+    public double AtomischeZwaarte;
+    // eventueel lijst met aantal electronen toevoegen om periodiek systeem goed te tonen
+    //public List<int> Electronen;
+
+    //boolean voor metaal, eventueel veranderen naar een type (niet-metalen, halogenen, edelgassen, etc)
+    public bool Metaal;
+
+    //boolean om te weten of vmbo-leerlingen deze stof uit hun hoofd moeten kennen
+    public bool LeerstofVmbo;
+    
+	public Element(int nummer, string afk, string naam, double zwaarte)
+    {
+        AtomischNummer = nummer;
+        Afkorting = afk;
+        Naam = naam;
+        AtomischeZwaarte = zwaarte;
+    }
+}
---
+++ Assets/Scripts/PeriodiekSysteem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PeriodiekSysteem {
+    public List<Element> Elementen;
+
+    public PeriodiekSysteem()
+    {
+        //de lijst van elementen wordt aangemaakt, alle gegevens hier gevonden: http://www.ptable.com/?lang=nl
+        Elementen = new List<Element>();
+        Element Waterstof = new Element(1, "H", "Waterstof", 1.008);
+        Elementen.Add(Waterstof);
+        Element Helium = new Element(2, "He", "Helium", 4.002602);
+        Elementen.Add(Helium);
+        Element Lithium = new Element(3, "Li", "Lithium", 6.94);
+        Elementen.Add(Lithium);
+        Element Beryllium = new Element(4, "Be", "Beryllium", 9.0121);
+        Elementen.Add(Beryllium);
+        Element Boor = new Element(5, "B", "Boor", 10.81);
+        Elementen.Add(Boor);
+    }
+
+}
